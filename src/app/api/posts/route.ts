b7d9204bdef55252af8d494d8307a7a155fb8296
HTG <namej5375@gmail.com>
2025-08-19 07:49:02 +0000

    javascript-event-loop.mdx" due to missing frontmatter (title, date, exce

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,5 +1,8 @@
+
 import { getPosts } from '@/lib/posts';
 import { NextResponse } from 'next/server';
+
+export const runtime = 'edge';
 
 export async function GET() {
   const posts = getPosts();
