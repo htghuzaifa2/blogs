0739768dd0b6fc7ec084790402f845037f588621
Huzi <huzi@huzi.pk>
2026-01-13 23:31:10 +0500

    Final Polish: Add robots.txt and verify content integrity

---
+++ src/app/robots.ts
@@ -0,0 +1,15 @@
+
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+    const siteUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://blogs.huzi.pk';
+
+    return {
+        rules: {
+            userAgent: '*',
+            allow: '/',
+            disallow: '/private/',
+        },
+        sitemap: `${siteUrl}/sitemap.xml`,
+    }
+}
